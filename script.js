5bf3040d432e017c15e8536b4e63e07fff62b32a
geraldosaidina <145325263+geraldosaidina@users.noreply.github.com>
2024-08-19 00:13:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarNome(){
+    let input = document.getElementById("nome");
+    let input2 = document.getElementById("email")
+    let nome = input.value;
+    let email = input.value;
+    document.getElementById("resultado").textContent = "Bem-vindo(a) ao site, " + nome + "." + email
+
+}
