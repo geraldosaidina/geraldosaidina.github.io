a7c0f77dab1770f7073d7c0286d1a6007997ce53
geraldosaidina <145325263+geraldosaidina@users.noreply.github.com>
2024-08-19 00:34:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function mostrarNome(){
     let input = document.getElementById("nome");
     let input2 = document.getElementById("email")
     let nome = input.value;
-    let email = input.value;
+    let email = input2.value;
     document.getElementById("resultado").textContent = "Bem-vindo(a) ao site, " + nome + "." + email
 
 }
